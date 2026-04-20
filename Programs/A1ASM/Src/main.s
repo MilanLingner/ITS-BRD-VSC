8debfee311525ffe8ba90497d37d2675340752b8
milan Lingner <milan.lingner@studium.uni-hamburg.de>
2026-04-20 20:00:44 +0200

    Korrektur A1

--- Programs/A1ASM/Src/main.s
+++ Programs/A1ASM/Src/main.s
@@ -34,7 +34,7 @@ main
 ;    MOV     R1, #0x02           ; load mask 0b0010
 ;    MOV     R2, #0x40           ; load mask 0b0100
 ;    MOV     R3, #0xAE           ; load mask 0b1000
-;    MOV     R4, #0x03           ; load mask 0b0011 
+    MOV     R4, #0x03           ; load mask 0b0011 
 
     ; Set LED
 ;    STRB    R2, [R6]    ; switch on LED D14
